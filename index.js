6945ff419affb3addb697e28b6e8f92bc3e4f8e3
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-18 10:40:21 -0600

    manager card created

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const managerArray = [];
 const internArray = [];
 const engineerArray = [];
 const mainArray = [];
+getMainInfo();
 
 function getMainInfo() {
   inquirer
     .prompt([
       {
         type: "input",
-        message: "Please name the project/team",
-        name: "project_name",
+        message: "What is the name of your project/team",
+        name: "project",
         validate: (value) => {
           if (value) {
             return true;
@@ -27,22 +28,191 @@ function getMainInfo() {
       },
     ])
     .then((answer) => {
-      mainArray.push();
+      mainArray.push(answer.project);
+      getEmployeeInfo;
     });
 }
 
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("Success!")
+function getEmployeeInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the team member name",
+        name: "name",
+      },
+      {
+        type: "list",
+        message: "What is their position?",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "position",
+      },
+      {
+        type: "input",
+        message: "What's their ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What's their email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What's the manager's office number?",
+        name: "officeNumber",
+        when: (answers) => answers.position === "Manager",
+      },
+      {
+        type: "input",
+        message: "What's the engineer's github account name?",
+        name: "github",
+        when: (answers) => answers.position === "Engineer",
+      },
+      {
+        type: "input",
+        message: "What's the intern's school?",
+        name: "school",
+        when: (answers) => answers.position === "Intern",
+      },
+    ])
+    .then((response) => {
+      if (response.position === "Manager") {
+        const manager = new Manager(
+          response.name,
+          response.id,
+          response.email,
+          response.officeNumber
+        );
+        managerArray.push(manager);
+      } else if (response.position === "Engineer") {
+        const engineer = new Engineer(
+          response.name,
+          response.id,
+          response.email,
+          response.github
+        );
+        engineerArray.push(engineer);
+      } else {
+        const intern = new Intern(
+          response.name,
+          response.id,
+          response.email,
+          response.school
+        );
+        internArray.push(intern);
+      }
+      addEmployee();
+    });
+}
+
+function managerCard() {
+  let managerInfo;
+  for (let i = 0; i < managerArray.length; i++) {
+    const info = managerArray[i];
+    managerInfo += `  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">${info.name}</span>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">${info.id}</li> 
+          <li class="list-group-item">${info.getOfficeNumber()}</li>
+        </ul>
+        </div>
+        <div class="card-action">
+          <a href="#">${info.email}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  }
+  return managerInfo;
+}
+function internCard() {
+  let internInfo;
+  for (let i = 0; i < internArray.length; i++) {
+    const info = internArray[i];
+    internInfo += `  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">${info.name}</span>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">${info.id}</li> 
+          <li class="list-group-item">${info.getOfficeNumber()}</li>
+        </ul>
+        </div>
+        <div class="card-action">
+          <a href="#">${info.email}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  }
+  return managerInfo;
+}
+function managerCard() {
+  let managerInfo;
+  for (let i = 0; i < managerArray.length; i++) {
+    const info = managerArray[i];
+    managerInfo += `  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">${info.name}</span>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">${info.id}</li> 
+          <li class="list-group-item">${data.getOfficeNumber()}</li>
+        </ul>
+        </div>
+        <div class="card-action">
+          <a href="#">${info.email}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  }
+  return managerInfo;
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add another person?",
+        name: "another",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((response) => {
+      if (response.another === "Yes") {
+        getEmployeeInfo();
+      } else {
+        generateHTML();
+      }
+    });
+}
+
+function generateHTML() {
+  fs.writeFile(
+    "./dist/index.html",
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    
+</body>
+</html>`
   );
 }
-
-function init() {
-  inquirer.prompt(questions);
-  // .then((data) => generateHtml(data))
-  // .then((data) => writeToFile('./dist/index.html', data))
-  // .then((data) => generateCss(data))
-  // .then((data) => writeToFile('./dist/style.css', data))
-}
-
-init();
